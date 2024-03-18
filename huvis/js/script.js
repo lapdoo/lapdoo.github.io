0af0c7d7c860593f843c8d8dcb951c810bf5c987
lapdoo <154848299+lapdoo@users.noreply.github.com>
2024-03-18 17:47:05 +0900

    Add files via upload

---
+++ huvis/js/script.js
@@ -0,0 +1,55 @@
+let gnb = document.querySelectorAll(".menu>li");
+let gnba = document.querySelectorAll(".menu>li>a");
+let sub = document.querySelectorAll(".sub");
+function menu_in() {
+  document.querySelector("header").classList.add("on");
+  gnba.forEach(function (value, index) {
+    value.classList.add("on");
+  });
+  document.querySelector(".logo img").src = "images/logo01_1_on.png";
+
+  sub.forEach(function (value, index) {
+    value.classList.add("on");
+  });
+  document.querySelector(".info").classList.add("on");
+}
+function menu_out() {
+  document.querySelector("header").classList.remove("on");
+  gnba.forEach(function (value, index) {
+    value.classList.remove("on");
+  });
+  document.querySelector(".logo img").src = "images/logo01_1.png";
+
+  sub.forEach(function (value, index) {
+    value.classList.remove("on");
+  });
+  document.querySelector(".info").classList.remove("on");
+}
+gnb.forEach(function (value, index) {
+  value.onmouseenter = function () {
+    menu_in();
+  };
+  value.onmouseleave = function () {
+    menu_out();
+  };
+  document.querySelector(".info").onmouseenter = menu_in;
+  document.querySelector(".info").onmouseleave = menu_out;
+});
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 6000,
+    pauseOnMouseEnter: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
