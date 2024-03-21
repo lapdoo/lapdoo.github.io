855613482ee2c4e8e297e27e90a95cdf7035e2a0
lapdoo <wlxodua@gmail.com>
2024-03-21 18:01:57 +0900

    first commit

--- amare/js/script.js
+++ amare/js/script.js
@@ -1,3 +1,11 @@
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $(".hd_fixed").addClass("active");
+  } else {
+    $(".hd_fixed").removeClass("active");
+  }
+});
+
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
--- metheus academy/js/script.js
+++ metheus academy/js/script.js
@@ -1,3 +1,11 @@
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $(".head").addClass("active");
+  } else {
+    $(".head").removeClass("active");
+  }
+});
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -9,5 +17,25 @@ var swiper = new Swiper(".mySwiper", {
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+  },
+});
+
+$(".class_btn p").click(function () {
+  let i = $(this).index();
+  $(".class_btn p span").css("backgroundColor", "#f3f3f3");
+  $(this).find("span").css("backgroundColor", "#000");
+  $(".class_btn_show ul").css("display", "none");
+  $(".class_btn_show ul").eq(i).css("display", "block");
 });
