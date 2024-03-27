9577d22c30d2ac982ec4fde6d4bf3d35efd50724
lapdoo <wlxodua@gmail.com>
2024-03-27 12:31:15 +0900

    first commit

--- huvis/js/script.js
+++ huvis/js/script.js
@@ -1,8 +1,46 @@
+let gnb = document.querySelectorAll(".menu>li");
+let gnba = document.querySelectorAll(".menu>li>a");
+let sub = document.querySelectorAll(".sub");
+function menu_in() {
+  document.querySelector("header").classList.add("on");
+  gnba.forEach(function (value, index) {
+    value.classList.add("on");
+  });
+  document.querySelector(".logo img").src = "images/logo01_1_on.png";
+
+  sub.forEach(function (value, index) {
+    value.classList.add("on");
+  });
+  document.querySelector(".info").classList.add("on");
+}
+function menu_out() {
+  document.querySelector("header").classList.remove("on");
+  gnba.forEach(function (value, index) {
+    value.classList.remove("on");
+  });
+  document.querySelector(".logo img").src = "images/logo01_1.png";
+
+  sub.forEach(function (value, index) {
+    value.classList.remove("on");
+  });
+  document.querySelector(".info").classList.remove("on");
+}
+gnb.forEach(function (value, index) {
+  value.onmouseover = function () {
+    menu_in();
+  };
+  value.onmouseleave = function () {
+    menu_out();
+  };
+  document.querySelector(".info").onmouseenter = menu_in;
+  document.querySelector(".info").onmouseleave = menu_out;
+});
+
 $(document).ready(function () {
   // fullpage
   $("#fullpage").fullpage({
     sectionsColor: ["#1A9C56", "#FFFCF5", "#7BAABE"],
-    anchors: ["sec1", "sec2", "sec3"],
+    anchors: ["sec1", "sec2", "sec3", "sec4", "sec5", "sec6"],
     menu: "#menu",
     scrollingSpeed: 1000,
     onLeave: function (origin, destination, direction) {
@@ -25,94 +63,54 @@ $(document).ready(function () {
   });
 
   // swiper
-  var length = $(".sec2 .swiper-slide").length;
-  var swiper = new Swiper(".swiper-container", {
+  var swiper = new Swiper(".sec1 .mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 0,
-    freeMode: false,
-    speed: 1000,
+    spaceBetween: 20,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      pauseOnMouseEnter: true,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    mousewheel: true,
-    on: {
-      slideChange: function () {
-        var idx = this.activeIndex;
-        // 처음과 마지막 슬라이드가 아닐경우 fullpage전환 막기
-        if (this.activeIndex != 0 && idx != length)
-          $.fn.fullpage.setAllowScrolling(false);
-        if (length == 2 && idx == 0) $.fn.fullpage.setAllowScrolling(false); //슬라이드가 2개밖에 없을때
-      },
-      slideChangeTransitionEnd: function () {
-        var idx = this.activeIndex;
-        // 처음과 마지막 슬라이드일 경우 fullpage전환 풀기
-        if (idx == 0 || idx >= length - 1)
-          $.fn.fullpage.setAllowScrolling(true);
-      },
-      touchMove: function (e) {
-        var startY = e.touches.startY;
-        setTimeout(function () {
-          if (startY > e.touches.currentY) swiper.slideNext();
-          else swiper.slidePrev();
-        }, 100);
-      },
-    },
+    // on: {
+    //   slideChange: function () {
+    //     let i = this.activeIndex;
+    //     console.log(i);
+    //     $(".innerWrap .inner p").removeClass("on");
+    //     $(".innerWrap .inner").eq(i).find("p").addClass("on");
+    //   },
+    // },
   });
 });
 
-// let gnb = document.querySelectorAll(".menu>li");
-// let gnba = document.querySelectorAll(".menu>li>a");
-// let sub = document.querySelectorAll(".sub");
-// function menu_in() {
-//   document.querySelector("header").classList.add("on");
-//   gnba.forEach(function (value, index) {
-//     value.classList.add("on");
+// $(document).ready(function () {
+//   $(".center").slick({
+//     centerMode: true,
+//     centerPadding: "60px",
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: "40px",
+//           slidesToShow: 3,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: "40px",
+//           slidesToShow: 1,
+//         },
+//       },
+//     ],
 //   });
-//   document.querySelector(".logo img").src = "images/logo01_1_on.png";
-
-//   sub.forEach(function (value, index) {
-//     value.classList.add("on");
-//   });
-//   document.querySelector(".info").classList.add("on");
-// }
-// function menu_out() {
-//   document.querySelector("header").classList.remove("on");
-//   gnba.forEach(function (value, index) {
-//     value.classList.remove("on");
-//   });
-//   document.querySelector(".logo img").src = "images/logo01_1.png";
-
-//   sub.forEach(function (value, index) {
-//     value.classList.remove("on");
-//   });
-//   document.querySelector(".info").classList.remove("on");
-// }
-// gnb.forEach(function (value, index) {
-//   value.onmouseover = function () {
-//     menu_in();
-//   };
-//   value.onmouseleave = function () {
-//     menu_out();
-//   };
-//   document.querySelector(".info").onmouseenter = menu_in;
-//   document.querySelector(".info").onmouseleave = menu_out;
 // });
-
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  effect: "fade",
-  autoplay: {
-    delay: 6000,
-    pauseOnMouseEnter: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
--- metheus academy/js/script.js
+++ metheus academy/js/script.js
@@ -39,16 +39,13 @@ $(window).on("load", function () {
 function setFlowBanner() {
   const $wrap = $(".flow_banner");
   const $list = $(".flow_banner .list");
-  let wrapWidth = ""; //$wrap의 가로 크기
-  let listWidth = ""; //$list의 가로 크기
-  const speed = 50; //1초에 몇픽셀 이동하는지 설정
-
-  //리스트 복제
+  let wrapWidth = "";
+  let listWidth = "";
+  const speed = 50;
   let $clone = $list.clone();
   $wrap.append($clone);
   flowBannerAct();
 
-  //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
   let oldWChk =
     window.innerWidth > 1279 ? "pc" : window.innerWidth > 767 ? "ta" : "mo";
   $(window).on("resize", function () {
@@ -60,9 +57,7 @@ function setFlowBanner() {
     }
   });
 
-  //배너 실행 함수
   function flowBannerAct() {
-    //배너 롤링 초기화
     if (wrapWidth != "") {
       $wrap.find(".list").css({
         animation: "none",
@@ -72,7 +67,6 @@ function setFlowBanner() {
     wrapWidth = $wrap.width();
     listWidth = $list.width();
 
-    //무한 반복을 위해 리스트를 복제 후 배너에 추가
     if (listWidth < wrapWidth) {
       const listCount = Math.ceil((wrapWidth * 2) / listWidth);
       for (let i = 2; i < listCount; i++) {
@@ -147,6 +141,6 @@ ScrollTrigger.create({
   start: "top 30%",
   end: "60% 50%",
   scrub: 2,
-  markers: true,
+  // markers: true,
   // pin: true,
 });
