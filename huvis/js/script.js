40c4f1c2dfcb7227da26dbb12709ce6cdf3679bc
lapdoo <wlxodua@gmail.com>
2024-03-28 18:00:26 +0900

    first commit

--- huvis/js/script.js
+++ huvis/js/script.js
@@ -88,33 +88,33 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function () {
-//   $(".center").slick({
-//     centerMode: true,
-//     centerPadding: "60px",
-//     slidesToShow: 3,
-//     responsive: [
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           arrows: false,
-//           centerMode: true,
-//           centerPadding: "40px",
-//           slidesToShow: 3,
-//         },
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           arrows: false,
-//           centerMode: true,
-//           centerPadding: "40px",
-//           slidesToShow: 1,
-//         },
-//       },
-//     ],
-//   });
-// });
+$(document).ready(function () {
+  $(".center").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
 
 $(".ft_drop>li").mouseenter(function () {
   $(this).children(".ft_hide").stop().slideDown(400);
